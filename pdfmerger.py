c4a7983d275bd309b49ba616b745b09071cf4513
tejas1597 <tjs1597@gmail.com>
2020-06-17 18:57:16 +0530

    Initial Upload

---
+++ pdfmerger.py
@@ -0,0 +1,16 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super2.pdf')
+    print(merger)
+
+
+pdf_combiner(inputs)
